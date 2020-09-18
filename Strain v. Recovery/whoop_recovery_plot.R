f209d97b024df28cbef4b03d6fad0f1526e73bf5
Michael Calabro <michael_calabro@college.harvard.edu>
2020-09-18 13:33:22 -0400

    Strain v Recovery plot made in R

---
+++ Strain v. Recovery/whoop_recovery_plot.R
@@ -0,0 +1,128 @@
+# Whoop Re-make Strain Vs Recovery
+library(tidyverse)
+library(gt)
+
+date <- (7:14)
+strain_loc <- c(.62, .97, .96, .97, .98, .97, .93, .65)
+strain <- c(13.7, 20.2, 19.5, 20.6, 20.7, 20.6, 19.2, 14.1)
+recovery <- c(.54, .90, .74, .68, .73, .43, .62, .74)
+
+data <- tibble(date) %>%
+          cbind(tibble(strain_loc)) %>%
+          cbind(tibble(strain)) %>%
+          cbind(tibble(recovery))
+
+data %>%
+  ggplot() +
+  geom_area(aes(x = date,
+                y = strain_loc),
+            color = "royalblue1",
+            fill = "royalblue1",
+            alpha = 0.08) +
+  geom_area(aes(x = date,
+                y = recovery),
+            color = "gray30",
+            fill = "black") +
+  geom_line(aes(x = date,
+                y = strain_loc),
+            color = "royalblue1") +
+  geom_point(aes(x = date,
+                 y = recovery,
+                 color = case_when(
+                   recovery < .34 ~ "red",
+                   recovery < .67 ~ "yellow",
+                   recovery > .66 ~ "green"
+                 )),
+             shape = 21, size = 4, stroke = 2, fill = "black") +
+  geom_point(aes(x = date,
+                 y = strain_loc),
+             shape = 21, size = 4, stroke = 2, fill = "black", color = "royalblue1") +
+  geom_text(data = data[1:7,], aes( x = date,
+                 y = recovery - 0.07,
+                 label = glue::glue("{recovery*100}%"),
+                 fontface = "bold",
+                 color = case_when(
+                   recovery < .34 ~ "red",
+                   recovery < .67 ~ "yellow",
+                   recovery > .66 ~ "green"
+                 ))) +
+  geom_text(data = data[1:7,], aes(x = date - 0.04,
+                y = strain_loc + 0.07,
+                label = strain,
+                fontface = "bold"),
+            color = "royalblue1") +
+  geom_text(data = data[8,], aes( x = date,
+                                  y = recovery + 0.07,
+                                  label = glue::glue("{recovery*100}%"),
+                                  fontface = "bold"),
+            color = "green") +
+  geom_text(data = data[8,], aes(x = date - 0.04,
+                                 y = strain_loc - 0.07,
+                                 label = strain,
+                                 fontface = "bold"),
+            color = "royalblue1") +
+  geom_segment(aes(x = 14.5, y = 0, xend = 14.5, yend = .32), color = "red") +
+  geom_segment(aes(x = 14.5, y = .34, xend = 14.5, yend = .65), color = "yellow") +
+  geom_segment(aes(x = 14.5, y = .67, xend = 14.5, yend = .99), color = "green") +
+  geom_segment(aes(x = 6, y = 0, xend = 6, yend = .15), color = "royalblue1") +
+  geom_segment(aes(x = 6, y = .17, xend = 6, yend = .32), color = "royalblue1") +
+  geom_segment(aes(x = 6, y = .34, xend = 6, yend = .485), color = "royalblue1") +
+  geom_segment(aes(x = 6, y = .5, xend = 6, yend = .65), color = "royalblue1") +
+  geom_segment(aes(x = 6, y = .67, xend = 6, yend = .82), color = "royalblue1") +
+  geom_segment(aes(x = 6, y = .84, xend = 6, yend = .985), color = "royalblue1") +
+  geom_text(aes(x = 15, y = 0, label = "0%"), color = "red") +
+  geom_text(aes(x = 15, y = .33, label = "33%"), color = "red") +
+  geom_text(aes(x = 15, y = .66, label = "66%"), color = "yellow") +
+  geom_text(aes(x = 15, y = 1, label = "100%"), color = "green") +
+  geom_text(aes(x = 6.35, y = 0, label = "0"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 1/6, label = "6"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 2/6, label = "10"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 3/6, label = "12"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 4/6, label = "14"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 5/6, label = "16"), color = "royalblue1") +
+  geom_text(aes(x = 6.35, y = 6/6, label = "21"), color = "royalblue1") +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.line.x = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_rect(fill = "black", color = "black"),
+        plot.background = element_rect(fill = "black", color = "black"),
+        panel.grid.minor.y = element_line(size = 0.015),
+        panel.grid.major.y = element_line(size = 0.015),
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(color = "white",
+                                   size = 11,
+                                   vjust = -1,
+                                   family = "sans",
+                                   face = "bold"),
+        plot.title = element_text(color = "white",
+                                  family = "sans",
+                                  face = "bold",
+                                  hjust = 0.5),
+        plot.subtitle = element_text(color = "white",
+                                  family = "sans",
+                                  face = "bold",
+                                  hjust = 0.5)) +
+  scale_y_continuous(limits = c(-.1, 1.1),
+                     position = "right") +
+  scale_x_continuous(breaks = (7:14),
+                     labels = c("Mon\n 7",
+                                "Tues\n 8",
+                                "Wed\n 9",
+                                "Thu\n 10",
+                                "Fri\n 11",
+                                "Sat\n 12",
+                                "Sun\n 13",
+                                "Mon\n 14")) +
+  coord_cartesian(xlim = c(4.5, 16), expand = FALSE) +
+  scale_color_manual(values = c("green", "yellow")) +
+  labs(title = "TEAM EF - STRAIN V. RECOVERY",
+       subtitle = "WEEK 2")
+
+
+
+
+
+
+
