6d4c89613b7d435d43e7dcd942e73a8744a1db95
Michael Calabro <michael_calabro@college.harvard.edu>
2020-09-20 18:57:49 -0400

    more plot progress

--- strain_recovery_python.py
+++ strain_recovery_python.py
@@ -7,17 +7,17 @@ recovery = [.54, .90, .74, .68, .73, .43, .62, .74]
 plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
-ax.fill_between(x, recovery, strain_loc, color="blue", alpha=0.08)
+ax.fill_between(x, recovery, strain_loc, color="#4b6cc8", alpha=0.1)
 ax.plot(x, recovery, color="gray", linewidth=0.8, alpha=0.5)
 ax.plot([8, 9, 10, 11, 14], [.90, .74, .68, .73, .74], 'go', markersize=10)
 ax.plot([7, 12, 13], [.54, .43, .62], 'yo', markersize=10)
 ax.plot(x, recovery, 'bo', color='black', markersize=6)
-ax.plot(x, strain_loc, color="blue", linewidth=1)
-ax.plot(x, strain_loc, 'bo', markersize=10)
-ax.plot(x, strain_loc, 'bo', color="black", markersize=6)
+ax.plot(x, strain_loc, color="#4b6cc8", linewidth=1, alpha=0.8)
+ax.plot(x, strain_loc, 'o', color="#4b6cc8", markersize=10, alpha=0.8)
+ax.plot(x, strain_loc, 'o', color="black", markersize=6)
 ax.axis([6, 15, 0, 1.1])
 ax.spines["right"].set_color("green")
-ax.spines["left"].set_color("blue")
+ax.spines["left"].set_color("#4b6cc8")
 ax.spines["top"].set_color("black")
 ax.spines["bottom"].set_color("black")
 plt.tick_params(
@@ -27,5 +27,3 @@ plt.tick_params(
     left=False
 )
 plt.show()
-
-
